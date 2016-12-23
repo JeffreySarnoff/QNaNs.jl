c5a04f999c660342009256b4615d62c170aa0eac
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2016-12-23 00:09:17 -0500

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,3 +1,10 @@
 using QNaNs
 using Base.test
 
+@test typeof(qnan(5)) == Float64
+@test typeof(qnan(Int32(5))) == Float32
+@test typeof(qnan(Int16(5))) == Float16
+@test qnan(qnan(-22)) == -22
+@test qnan(qnan(Int32(-22))) == -22
+@test qnan(qnan(Int16(-22))) == -22
+
