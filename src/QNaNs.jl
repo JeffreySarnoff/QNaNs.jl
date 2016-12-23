1b967b6ebce7113643594ad528e400cf0869ac06
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2016-12-22 23:33:42 -0500

    eliminate extraneous functions, allow zero payload

--- src/QNaNs.jl
+++ src/QNaNs.jl
@@ -25,20 +25,12 @@ for (FL, I, UI, UPos, UNeg) in [(:Float64, :Int64, :UInt64, :0x7ff8000000000000,
                                 (:Float32, :Int32, :UInt32, :0x7fc00000, :0xffc00000),
                                 (:Float16, :Int16, :UInt16, :0x7e00, :0xfe00) ]
   @eval begin
-      @inline isjnan(x::$(UI))    = (x == $(UNeg)) || (x == $(UPos))
       @inline isqnan(x::$(UI))    = (x & $(UPos)) == $(UPos)
-      @inline isqnanPos(x::$(UI)) = (x & $(UNeg)) == $(UPos)
-      @inline isqnanNeg(x::$(UI)) = (x & $(UNeg)) == $(UNeg)
-      @inline isjnan(x::$(FL))    = isjnan(reinterpret($(UI),x))
       @inline isqnan(x::$(FL))    = isqnan(reinterpret($(UI),x))
-      @inline isqnanPos(x::$(FL)) = isqnanPos(reinterpret($(UI),x))
-      @inline isqnanNeg(x::$(FL)) = isqnanNeg(reinterpret($(UI),x))
 
       function qnan(si::$(I))
           u = reinterpret($(UI), abs(si))
-          if (u & ~$(UPos)) == 0
-              throw(ArgumentError("The magnitude of n cannot be 0, that NaN is used by Julia."))
-          elseif (u > ~$(UNeg)) # 2^51-1
+          if (u > ~$(UNeg)) # 2^51-1
               throw(ArgumentError("The magnitude of n $(si) exceeds available QNaN."))
           end
           u |= ((si > 0) ? $(UPos) : $(UNeg))
@@ -58,13 +50,18 @@ for (FL, I, UI, UPos, UNeg) in [(:Float64, :Int64, :UInt64, :0x7ff8000000000000,
 end
 
 @doc """
-Module QNaN
   **qnan**(`si`::{Int16|32|64}) generates a quiet NaN with a payload of `si`
   **qnan**(`fp`::FloatingPoint) recovers the Signed payload from `fp`
   **isqnan**(`fp`::FloatingPoint) true iff `fp` is any QNaN
-  **isqnanPos**(`fp`::FloatingPoint) true iff isqnan(`fp`) and !signbit(`fp`)
-  **isqnanNeg**(`fp`::FloatingPoint) true iff isqnan(`fp`) and  signbit(`fp`)
-  **isjnan**(`fp`::FloatingPoint) true iff `fp` is a QNaN that Julia uses
-""" -> QNaN
+""" -> QNaNs
+
+@doc """
+  **qnan**(`si`::{Int16|32|64}) generates a quiet NaN with a payload of `si`
+  **qnan**(`fp`::FloatingPoint) recovers the Signed payload from `fp`
+""" -> qnan
+
+@doc """
+  **isqnan**(`fp`::FloatingPoint) true iff `fp` is any QNaN
+""" -> isqnan
 
 end # module
