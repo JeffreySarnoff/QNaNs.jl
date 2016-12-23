049836e278559174b0bcdf3eeeec334910032298
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2016-12-23 00:11:05 -0500

    add tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,9 +1,14 @@
 using QNaNs
-using Base.test
+using Base.Test
+
+@test isnan(qnan(5)) == true
+@test isnan(qnan(Int32(5))) == true
+@test isnan(qnan(Int16(5))) == true
 
 @test typeof(qnan(5)) == Float64
 @test typeof(qnan(Int32(5))) == Float32
 @test typeof(qnan(Int16(5))) == Float16
+
 @test qnan(qnan(-22)) == -22
 @test qnan(qnan(Int32(-22))) == -22
 @test qnan(qnan(Int16(-22))) == -22
