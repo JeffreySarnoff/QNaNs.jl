7b77a79200d64c5073448a0c2fe35f40a738cc0c
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2017-01-19 02:05:38 -0500

    test qnan(0)===NaN

--- test/runtests.jl
+++ test/runtests.jl
@@ -9,7 +9,13 @@ using Base.Test
 @test typeof(qnan(Int32(5))) == Float32
 @test typeof(qnan(Int16(5))) == Float16
 
+@test qnan(qnan(Int64(17))) == 0
+@test qnan(qnan(Int32(17))) == 0
+@test qnan(qnan(Int16(17))) == 0
+
 @test qnan(qnan(-22)) == -22
 @test qnan(qnan(Int32(-22))) == -22
 @test qnan(qnan(Int16(-22))) == -22
 
+@test qnan(0) === NaN
+@test qnan(0%Int32) === NaN32
